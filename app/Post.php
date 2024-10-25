c628e2c1cd30ab211c13f7d139ec18bb52237eee
Soli <soli@gmail.com>
2024-10-25 19:58:59 +0330

    fix : use tableNames in post, postStatus model & set relationships of their

--- app/Post.php
+++ app/Post.php
@@ -4,12 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $table = 'posts';
 
     protected $fillable = [
         'post_status_id',
@@ -19,4 +22,9 @@ class Post extends Model
         'updated_at',
         'deleted_at'
     ];
+
+    public function postStatus(): BelongsTo
+    {
+        return $this->belongsTo(PostStatus::class);
+    }
 }
--- app/PostStatus.php
+++ app/PostStatus.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostStatus extends Model
@@ -11,8 +12,15 @@ class PostStatus extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $table = 'post_status';
+
     protected $fillable = [
         'code',
         'title'
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
