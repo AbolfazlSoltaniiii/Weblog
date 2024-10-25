8228d42510b372332c8658b9a0c7270fdaad0212
Soli <soli@gmail.com>
2024-10-25 20:01:55 +0330

    feat : add route for get posts data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', static function () {
     return view('app');
 });
+
+Route::post('post/index', [PostController::class, 'index']);
