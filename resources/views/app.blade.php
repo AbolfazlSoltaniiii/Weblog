bf2d6dd964638b6773fea52146587b4ca5f31e91
Soli <soli@gmail.com>
2025-01-30 11:46:40 +0330

    feat : add weblog icon & set for website icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <link rel="icon" href="{{asset('icon/weblog.png')}}">
+
     <style>
         body {
             font-family: "B Yekan", sans-serif;
--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <link rel="icon" href="{{asset('icon/weblog.png')}}">
+
     <style>
         * {
             font-family: "B Yekan", sans-serif;
